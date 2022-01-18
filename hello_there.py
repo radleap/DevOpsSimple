7158862e592b5f365293544ff015d4ddfb55525c
radleap <guy@radleap.com>
2022-01-17 21:28:04 -0500

    Updating the python script code to use requests package and ping google.com

--- hello_there.py
+++ hello_there.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
-print('hello there!')
+import requests
+x = requests.get('https://www.google.com')
+
+if x.status_code == 200:
+    print("Grreeeeat it works!!! Code 200! ")
+else:
+    print("Bummer the code wasn't 200!! ")
